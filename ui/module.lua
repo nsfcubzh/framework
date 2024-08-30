288dcfe8073f87364f2898135199b8771a8c7aa7
SysFAB <simplehuman228@gmail.com>
2024-08-30 10:53:07 +0300

    add ui module

---
+++ ui/module.lua
@@ -0,0 +1,17 @@
+-- UI
+-- NSFWorks UI
+
+local ui_loader = {}
+
+ui_loader.init = function(self, e)
+	self.env = e
+    rawset(self.env, "UI", self:new())
+end
+
+ui_loader.new = function(loader)
+    local ui = {}
+
+    return ui
+end
+
+return ui_loader
