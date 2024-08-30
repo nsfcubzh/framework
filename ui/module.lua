dc8f975ee1f4b04e1b3fd4dd58e9793222661c4a
SysFAB <simplehuman228@gmail.com>
2024-08-30 11:40:42 +0300

    node system rewrite

--- ui/module.lua
+++ ui/module.lua
@@ -40,14 +40,35 @@ local ui_mt = {
             end
         elseif self.loaded[key] ~= nil then
             return function(...)
-                local o = self.loader.createBaseNode(self.uikit)
+                local o = {} 
+                o.uikit_node = self.uikit:createNode()
+                o.initialized = false
+
+                setmetatable(o, {
+                    __index = function(self, key)
+                        if key == "Position" then
+                            return rawget(self, "uikit_node").pos
+                        elseif key == "uikit_object" then
+                            return rawget(self, "uikit_node")
+                        elseif rawget(self, "initialized") == false then
+                            return rawget(self, key)
+                        end
+                    end,
+                    __newindex = function(self, key, value)
+                        if key == "Position" then
+                            rawget(self, "uikit_node").pos = value
+                        elseif rawget(self, "initialized") == false then
+                            return rawset(self, key, value)
+                        end
+                    end
+                })
 
                 o.Remove = self.loaded[key].Remove
                 o.Update = self.loaded[key].Update
                 o.Tick = self.loaded[key].Tick
 
                 o.initialized = true
-                
+
                 self.loaded[key].Create(o, ...)
 
                 return o
@@ -72,38 +93,6 @@ ui_loader.new = function(loader)
     setmetatable(ui, ui_mt)
 
     return ui
-end
-
-ui_loader.createBaseNode = function(uikit)
-    local node = {}
-
-    node.uikit_node = uikit:createNode()
-    node.initialized = false
-
-    setmetatable(node, {
-        __index = function(self, key)
-            if key == "Position" then
-                return self.uikit_node.pos
-            elseif key == "SetParent" then
-                return self.uikit_node.setParent
-            elseif key == "AddChild" then
-                return self.uikit_node.addChild
-            elseif key == "uikit_object" then
-                return self.uikit_node
-            elseif self.initialized == false then
-                return rawget(self, key)
-            end
-        end,
-        __newindex = function(self, key, value)
-            if key == "Position" then
-                self.uikit_node.pos = value
-            elseif self.initialized == false then
-                rawset(self, key, value)
-            end
-        end
-    })
-
-    return node
 end
 
 --http://lua-users.org/wiki/CopyTable
